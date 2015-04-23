151da086ca5b4a9b0faf0c02c448e0ac43e09b8b
Dennis Charles Hackethal <dennis@generalassemb.ly>
2015-04-23 12:21:28 -0700

    Implement Car

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
 class Car
+	attr_reader :speed
+
+	def initialize
+		@speed = 0
+	end
+
+	def accelerate(gain)
+		@speed += gain
+	end
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe Car do
-	
+	describe '#initialize' do
+		it 'has a default speed of 0' do
+			car = Car.new
+			expect(car.speed).to eq 0
+		end
+	end
+
+	it 'can accelerate' do
+		car = Car.new
+
+		expect {
+			car.accelerate(20)
+		}.to change(car, :speed).by(20)
+	end
 end
